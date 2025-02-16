119643978f3100c34ef34818ffe7380e1a3e20e2
faustro <ferdysuper171@gmail.com>
2025-02-16 14:54:44 +0800

    Fix: not delete channel after buying message

--- commands/buy.js
+++ commands/buy.js
@@ -29,204 +29,117 @@ module.exports = {
       return message.reply('This command can only be used in a guild.');
     }
 
-    // Check if the channel where the command was used is a ticket channel
-    // if (!message.channel.name.startsWith('ticket-')) {
-    //   return message.reply('This command can only be used in a ticket channel.');
-    // }
-
-    // Check if there's a GrowID provided in the command
     if (args.length < 2) {
       return message.reply('Please provide both the code of the product and the quantity you want to buy.');
     }
 
     const productCode = args[0];
     const quantity = parseInt(args[1]);
-    const discordId = message.author.id; // Get the user's Discord ID
+    const discordId = message.author.id; 
 
     const logChannel = message.guild.channels.cache.get(buylogChannelId);
 
     if (isNaN(quantity) || quantity <= 0) {
       return message.reply('Please provide a valid quantity greater than 0.');
     }
+
     try {
       let purchasedAccounts = [];
-      // Check if the user already exists in the database
       const user = await User.findOne({ discordId });
 
       if (!user) {
         return message.reply('You need to set your GrowID using the `.set` command first.');
       }
 
-      // Find the product in the database by code
       const product = await Product.findOne({ code: productCode });
 
       if (!product) {
         return message.reply('This product does not exist.');
       }
 
-      // Check if there are variations (account details) available
       if (!product.variations || product.variations.length === 0) {
         return message.reply('There are no product details available for this product.');
       }
 
-      // Check if there's enough stock to buy
       if (product.stock < quantity) {
         return message.reply(`There is not enough stock to purchase ${quantity} of this product.`);
       }
 
-      // Calculate the total price based on the quantity of products
       const totalPrice = product.price * quantity;
 
-      // Check if the user has enough balance to make the purchase
       if (user.balance < totalPrice) {
         return message.reply('You do not have enough balance to purchase this quantity of the product.');
       }
 
-      // Check the product type
       switch (product.type) {
-        // Inside the 'buy' command
-case 'yes':
-  // Handle "yes" type using the asynchronous function
-  await handleYesType(user, message, product, quantity);
-  break;
+        case 'yes':
+          await handleYesType(user, message, product, quantity);
+          break;
 
         case 'no':
-  // For "any-product" type, reduce both stock and details
-  if (product.variations.length < quantity) {
-    return message.reply(`There are only **${product.variations.length} ${product.name}** available for purchase.`);
-  }
-  purchasedAccounts = [];
-  const randomIndexes = [];
-  
-  for (let i = 0; i < quantity; i++) {
-    // Generate a random index
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * product.variations.length);
-    } while (randomIndexes.includes(randomIndex)); // Ensure we don't select the same detail twice
-    
-    randomIndexes.push(randomIndex);
-    
-    const selectedVariation = product.variations[randomIndex];
-    purchasedAccounts.push(selectedVariation);
-  }
-  // Remove the purchased details from the product's variations
-  product.variations = product.variations.filter((_, index) => !purchasedAccounts.includes(product.variations[index]));
+        case 'df': {
+          if (product.variations.length < quantity) {
+            return message.reply(`There are only **${product.variations.length} ${product.name}** available for purchase.`);
+          }
+
+          purchasedAccounts = [];
+          const randomIndexes = [];
           
-  // Update the stock count
-  product.stock -= quantity;
+          for (let i = 0; i < quantity; i++) {
+            let randomIndex;
+            do {
+              randomIndex = Math.floor(Math.random() * product.variations.length);
+            } while (randomIndexes.includes(randomIndex));
+            
+            randomIndexes.push(randomIndex);
+            purchasedAccounts.push(product.variations[randomIndex]);
+          }
 
-  // Save the updated product to the database
-  await product.save();
+          product.variations = product.variations.filter((_, index) => !randomIndexes.includes(index));
+          product.stock -= quantity;
+          await product.save();
+          purchaseEmitter.emit('purchase');
 
-  // Emit the 'purchaseMade' event to trigger real-time stock update
-  purchaseEmitter.emit('purchase');
+          const detailsMessage = purchasedAccounts.join('\n\n\n');
+          const fileName = `${user.growId}.txt`;
 
-  const detailsMessage = purchasedAccounts.join('\n');
-  const fileName = `${user.growId}.txt`;
+          fs.writeFileSync(fileName, detailsMessage);
 
-  // Create the details file
-  fs.writeFileSync(fileName, detailsMessage);
+          const embedDM = new EmbedBuilder()
+            .setColor('#007fff')
+            .setTitle('Purchase Successful')
+            .setDescription(`You have purchased **${quantity} ${product.name.replace(/"/g, '')}** worth **${totalPrice}${wlEmoji}**\n**Don't forget to give reps.**\n`)
+            .setImage(imageURL)
+            .setTimestamp();
 
-  // Send the file to the user via DM
-  const embedDM = new EmbedBuilder()
-    .setColor('#007fff')
-    .setTitle('Purchase Successful')
-    .setDescription(`You have purchased **${quantity} ${product.name.replace(/"/g, '')}** worth **${totalPrice}${wlEmoji}**\n**Don't forget to give reps.**\n`)
-    .setImage(imageURL)
-    .setTimestamp();
-  await message.author.send({ embeds: [embedDM], files: [fileName] });
-
-  // Delete the file after sending
-  fs.unlinkSync(fileName);
-  break;
-
-        case 'df':
-          // Handle "df" type to send random details from the database via DM
-           if (product.variations.length < quantity) {
-    return message.reply(`There are only **${product.variations.length} ${product.name}** available for purchase.`);
-  }
-  purchasedAccounts = [];
-  const randomIndexess = [];
-  
-  for (let i = 0; i < quantity; i++) {
-    // Generate a random index
-    let randomIndex;
-    do {
-      randomIndex = Math.floor(Math.random() * product.variations.length);
-    } while (randomIndexess.includes(randomIndex)); // Ensure we don't select the same detail twice
-    
-    randomIndexess.push(randomIndex);
-    
-    const selectedVariation = product.variations[randomIndex];
-    purchasedAccounts.push(selectedVariation);
-  }
-  // Remove the purchased details from the product's variations
-  product.variations = product.variations.filter((_, index) => !purchasedAccounts.includes(product.variations[index]));
-          
-  // Update the stock count
-  product.stock -= quantity;
-
-  // Save the updated product to the database
-  await product.save();
-
-  // Emit the 'purchaseMade' event to trigger real-time stock update
-  purchaseEmitter.emit('purchase');
-
-  const detailssMessage = purchasedAccounts.join('\n\n\n');
-  const fileNames = `${user.growId}.txt`;
-
-  // Create the details file
-  fs.writeFileSync(fileNames, detailssMessage);
-
-  // Send the file to the user via DM
-  const embedDMs = new EmbedBuilder()
-    .setColor('#007fff')
-    .setTitle('Purchase Successful')
-    .setDescription(`You have purchased **${quantity} ${product.name.replace(/"/g, '')}** worth **${totalPrice}${wlEmoji}**\n**Don't forget to give reps.**\n`)
-    .setImage(imageURL)
-    .setTimestamp();
-  await message.author.send({ embeds: [embedDMs], files: [fileNames] });
-
-  // Delete the file after sending
-  fs.unlinkSync(fileNames);
+          await message.author.send({ embeds: [embedDM], files: [fileName] });
+          fs.unlinkSync(fileName);
           break;
+        }
+
         case 'autosend':
-          // Handle autosend type (customize as needed)
-          // For example, send the purchased product directly here
           await autosendFunction(user, message, product, quantity);
           break;
+
         default:
           return message.reply('This product type is not supported.');
       }
 
-      // Save the updated product to the database
       await product.save();
-
-      // Deduct the total price from the user's balance
       user.balance -= totalPrice;
       await user.save();
 
-      // Fetch the current orderCount
       orderCount = await getOrderCount();
-
-      // Increment the order count
       orderCount++;
-
-      // Update the order count in the database
       await OrderCount.findOneAndUpdate({}, { count: orderCount }, { upsert: true });
 
-      // Emit the 'purchaseMade' event to trigger real-time stock update
       purchaseEmitter.emit('purchase');
 
-      // Add the role to the user
       const roleToAdd = message.guild.roles.cache.get(product.roleToadd);
-
       if (roleToAdd) {
         await message.member.roles.add(roleToAdd);
       }
 
-      // Create the purchase log embed
       const purchaseLogEmbed = new EmbedBuilder()
         .setColor('#007fff')
         .setTitle(`Order Number: **${orderCount}**`)
@@ -237,12 +150,10 @@ ${emoji1} Total Price: **${totalPrice}** ${wlEmoji}\n\n**Thanks For Purchasing O
         .setImage(imageURL)
         .setTimestamp();
 
-      // Send the purchase log to the log channel
       if (logChannel) {
         logChannel.send({ embeds: [purchaseLogEmbed] });
       }
 
-      // Send the purchase confirmation message to the user
       const purchaseConfirmationEmbed = new EmbedBuilder()
         .setColor('#007fff')
         .setTitle('Purchase Successful')
@@ -251,31 +162,24 @@ ${emoji1} Total Price: **${totalPrice}** ${wlEmoji}\n\n**Thanks For Purchasing O
 
       message.reply({ embeds: [purchaseConfirmationEmbed] })
         .then(() => {
-          // Delay the channel deletion to allow the user to see the messages and send a warning message
-          const delayBeforeDeletion = 10000; // 10 seconds
+          const delayBeforeWarning = 10000;
           setTimeout(() => {
-            // Check if the channel exists before attempting to delete it
             if (message.channel) {
-              message.channel.send(`This channel will be deleted in **${delayBeforeDeletion / 1000}** seconds. Please check your DM!`)
-                .then(() => {
-                  setTimeout(() => {
-                    // Check if the channel exists again before attempting to delete it
-                    if (message.channel) {
-                      message.channel.delete()
-                        .catch(console.error);
-                    }
-                  }, delayBeforeDeletion);
-                });
+              message.channel.send(`This channel will remain open. Please check your DM!`);
             }
-          }, 1000); // Delay the warning message by 1 second
+          }, 1000);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          return message.reply('Something went wrong.');
         });
+
     } catch (error) {
       console.error('Error:', error);
       return message.reply('Something went wrong.');
     }
-  },
+  }
 };
-
 
 async function autosendFunction(user, message, product, quantity) {
   // Customize this function to handle autosend products
