7447cc331f04e0380fcc36962345c2aebf018090
faustro <ferdysuper171@gmail.com>
2025-02-16 08:03:09 +0800

    Initialize

---
+++ commands/setmt.js
@@ -0,0 +1,21 @@
+const { ownerid } = require('../config.json');
+
+let maintenanceMode = false;
+
+module.exports = {
+  name: 'setmt',
+  description: 'Toggle maintenance mode for the bot.',
+  execute(message, args) {
+    // Check if the user executing the command has the allowed user ID
+    if (message.author.id !== ownerid) {
+      return message.reply('You do not have permission to use this command.');
+    }
+
+    // Toggle maintenance mode
+    maintenanceMode = !maintenanceMode;
+
+    // Send a response message indicating the current maintenance status
+    message.channel.send(`Maintenance mode is now ${maintenanceMode ? 'enabled' : 'disabled'}.`);
+  },
+  isMaintenanceModeEnabled: () => maintenanceMode,
+};
